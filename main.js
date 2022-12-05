e5f55a31c9872b43ac6744722f8ca4d85196fe97
Diego19890102 <diegomonfort1989@gmail.com>
2022-12-05 02:08:29 +0100

    d
    d

---
+++ main.js
@@ -0,0 +1,41 @@
+
+const links = document.querySelectorAll('.nav-item a');
+const activeNav = document.querySelector('.active-nav');
+
+
+
+links.forEach((link) => {
+    link.addEventListener('click', ()=>{
+        //nav Blue
+        gsap.to(links, {color: '#252525'});
+         if(document.activeElement === link){
+            gsap.to(link, {color: '#385ae0'});
+        }
+
+//move the line
+        const state = Flip.getState(activeNav);
+        link.appendChild(activeNav);
+        Flip.from(state, {
+            duration:1.25,
+            absolute: true,
+            ease:'elastic.out(1,0.5)'
+        });
+
+    });
+
+   
+});
+
+//cards
+const cards = document.querySelectorAll('.card');
+cards.forEach((card, index) => {
+    card.addEventListener('click',()=>{
+        const state = Flip.getState(cards);
+
+
+        const isCardActive = card.classList.contains('active');
+        cards.forEach((otherCard, otherIndex) =>{
+            otherCard.classList.remove('active');
+        })
+    }
+})
